b20785802ae3476808723e9c52467d738421c779
David Amebley <davameb@gmail.com>
2025-02-04 13:57:10 +0200

    Add basic template structure with color and brush size inputs

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="toolbar">
+        <label for="colorPicker">Color:</label>
+        <input
+            id="colorPicker"
+            type="color"
+            :value="color"
+            @input="handleColorChange"
+        />
+
+        <label for="brushSize">Brush Size:</label>
+        <input
+            id="brushSize"
+            type="range"
+            min="1"
+            max="50"
+            :value="brushSize"
+            @input="handleBrushSizeChange"
+        />
+    </div>
+</template>
